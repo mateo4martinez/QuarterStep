e11c078824f7e89df6b1a863ac39b9af900b0383
Mateo Martinez <mateom@fb.com>
2021-07-13 09:07:42 -0700

    built loginActivity layout

---
+++ app/src/main/java/com/codepath/quarterstep/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.codepath.quarterstep.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.codepath.quarterstep.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
