c0a69c46648a41876c827a8bf7d5c7e12e474029
Mateo Martinez <mateom@fb.com>
2021-07-14 13:06:23 -0700

    added animating navigation bar

--- app/src/main/java/com/codepath/quarterstep/activities/MainActivity.java
+++ app/src/main/java/com/codepath/quarterstep/activities/MainActivity.java
@@ -1,6 +1,8 @@
 package com.codepath.quarterstep.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,7 +10,14 @@ import android.view.View;
 import android.widget.Button;
 
 import com.codepath.quarterstep.R;
+import com.codepath.quarterstep.utils.ScreenSlidePageFragment;
+import com.codepath.quarterstep.utils.ScreenSlidePagerAdapter;
+import com.gauravk.bubblenavigation.BubbleNavigationLinearView;
+import com.gauravk.bubblenavigation.listener.BubbleNavigationChangeListener;
 import com.parse.ParseUser;
+
+import java.io.FileReader;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnLogout = findViewById(R.id.btnLogout);
-
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,6 +38,40 @@ public class MainActivity extends AppCompatActivity {
                 Intent i = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(i);
                 finish();
+            }
+        });
+
+        ArrayList<ScreenSlidePageFragment> fragments = new ArrayList<>();
+        fragments.add(ScreenSlidePageFragment.newInstance(getString(R.string.arrangement), R.color.red_inactive));
+        fragments.add(ScreenSlidePageFragment.newInstance(getString(R.string.home), R.color.blue_inactive));
+        fragments.add(ScreenSlidePageFragment.newInstance(getString(R.string.profile), R.color.green_inactive));
+
+        ScreenSlidePagerAdapter pagerAdapter = new ScreenSlidePagerAdapter(fragments, getSupportFragmentManager());
+
+        final BubbleNavigationLinearView bubbleNavigationLinearView = findViewById(R.id.bottomNavigationBar);
+        final ViewPager viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                bubbleNavigationLinearView.setCurrentActiveItem(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        bubbleNavigationLinearView.setNavigationChangeListener(new BubbleNavigationChangeListener() {
+            @Override
+            public void onNavigationChanged(View view, int position) {
+                viewPager.setCurrentItem(position, true);
             }
         });
     }
---
+++ app/src/main/java/com/codepath/quarterstep/utils/ScreenSlidePageAdapter.kt
@@ -0,0 +1,20 @@
+package com.codepath.quarterstep.utils
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ScreenSlidePagerAdapter(
+        private val fragmentList: ArrayList<ScreenSlidePageFragment>,
+        fm: FragmentManager
+) : FragmentStatePagerAdapter(fm) {
+
+    override fun getCount(): Int = fragmentList.size
+
+    override fun getItem(position: Int): Fragment {
+        if (position >= 0 && position < fragmentList.size)
+            return fragmentList[position]
+        return ScreenSlidePageFragment()
+    }
+}
