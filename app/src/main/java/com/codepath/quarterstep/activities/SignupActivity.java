36922e44190567881f9d14d0ece4ff3b5faa20ae
Mateo Martinez <mateom@fb.com>
2021-07-13 20:58:50 -0700

    Users can now sign up for an account

--- app/src/main/java/com/codepath/quarterstep/activities/SignupActivity.java
+++ app/src/main/java/com/codepath/quarterstep/activities/SignupActivity.java
@@ -2,15 +2,97 @@ package com.codepath.quarterstep.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.codepath.quarterstep.R;
+import com.codepath.quarterstep.User;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class SignupActivity extends AppCompatActivity {
+
+    public static final String TAG = "SignupActivity";
+
+    private EditText etFirstName;
+    private EditText etLastName;
+    private EditText etUsername;
+    private EditText etPassword;
+    private EditText etEmail;
+    private Button btnSignup;
+    private Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        etFirstName = findViewById(R.id.etFirstName);
+        etLastName = findViewById(R.id.etLastName);
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        etEmail = findViewById(R.id.etEmail);
+        btnSignup = findViewById(R.id.btnSignup);
+        btnBack = findViewById(R.id.btnBack);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstName = etFirstName.getText().toString();
+                String lastName = etLastName.getText().toString();
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                String email = etEmail.getText().toString();
+                signupUser(firstName, lastName, username, password, email);
+            }
+        });
+    }
+
+    private void signupUser(String firstName, String lastName, String username, String password, String email) {
+        User user = new User(new ParseUser());
+
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(email);
+
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Signing up went wrong", e);
+                    return;
+                }
+                loginUser(username, password);
+            }
+        });
+    }
+
+    private void loginUser(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with login", e);
+                    Toast.makeText(SignupActivity.this, "Issue with login.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                goMainActivity();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
