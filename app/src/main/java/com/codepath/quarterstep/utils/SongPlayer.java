4e1564d6130c2f73937fd57e85bca360dcefac16
Mateo Martinez <mateom@fb.com>
2021-07-19 15:23:27 -0700

    can now play a song

--- app/src/main/java/com/codepath/quarterstep/utils/SongPlayer.java
+++ app/src/main/java/com/codepath/quarterstep/utils/SongPlayer.java
@@ -2,152 +2,96 @@ package com.codepath.quarterstep.utils;
 
 import android.content.Context;
 import android.media.MediaPlayer;
+import android.util.Log;
 
 import com.codepath.quarterstep.R;
 import com.codepath.quarterstep.models.Note;
 import com.codepath.quarterstep.models.Song;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 
 public class SongPlayer {
+    public static final String TAG = "SongPlayer";
+
     private Context context;
-    private MediaPlayer mediaPlayer;
+    private MySoundPool soundPool;
+    private Song song;
 
-    public SongPlayer(Context context) {
+    public SongPlayer(Context context, MySoundPool soundPool, Song song) {
         this.context = context;
+        this.soundPool = soundPool;
+        this.song = song;
     }
 
-    public void createNote(Note note) {
-        String noteName = note.getNoteName();
-        switch (noteName.charAt(0)) {
-            case 'C':
-                switch (noteName.charAt(1)) {
-                    case '_':
-                        switch (noteName.charAt(2)) {
-                            case '4':
-                                mediaPlayer = MediaPlayer.create(context, R.raw.c_4);
-                            case '5':
-                                mediaPlayer = MediaPlayer.create(context, R.raw.c_5);
-                        }
-                    default:
-                        switch (noteName.charAt(2)) {
-                            case '4':
-                                mediaPlayer = MediaPlayer.create(context, R.raw.c4);
-                            case '5':
-                                mediaPlayer = MediaPlayer.create(context, R.raw.c5);
-                        }
-                }
-            case 'D':
-                switch (noteName.charAt(1)) {
-                    case '_':
-                        switch (noteName.charAt(2)) {
-                            case '4':
-                                mediaPlayer = MediaPlayer.create(context, R.raw.d_4);
-                            case '5':
-                                mediaPlayer = MediaPlayer.create(context, R.raw.d_5);
-                        }
-                    default:
-                        switch (noteName.charAt(2)) {
-                            case '4':
-                                mediaPlayer = MediaPlayer.create(context, R.raw.d4);
-                            case '5':
-                                mediaPlayer = MediaPlayer.create(context, R.raw.d5);
-                        }
-                }
-            case 'E':
-                switch (noteName.charAt(1)) {
-                    case '4':
-                        mediaPlayer = MediaPlayer.create(context, R.raw.e4);
-                    case '5':
-                        mediaPlayer = MediaPlayer.create(context, R.raw.e5);
-                }
-            case 'F':
-                switch (noteName.charAt(1)) {
-                    case '_':
-                        switch (noteName.charAt(2)) {
-                            case '4':
-                                mediaPlayer = MediaPlayer.create(context, R.raw.f_4);
-                            case '5':
-                                mediaPlayer = MediaPlayer.create(context, R.raw.f_5);
-                        }
-                    default:
-                        switch (noteName.charAt(2)) {
-                            case '4':
-                                mediaPlayer = MediaPlayer.create(context, R.raw.f4);
-                            case '5':
-                                mediaPlayer = MediaPlayer.create(context, R.raw.f5);
-                        }
-                }
-            case 'G':
-                switch (noteName.charAt(1)) {
-                    case '_':
-                        switch (noteName.charAt(2)) {
-                            case '4':
-                                mediaPlayer = MediaPlayer.create(context, R.raw.g_4);
-                            case '5':
-                                mediaPlayer = MediaPlayer.create(context, R.raw.g_5);
-                        }
-                    default:
-                        switch (noteName.charAt(2)) {
-                            case '4':
-                                mediaPlayer = MediaPlayer.create(context, R.raw.g4);
-                            case '5':
-                                mediaPlayer = MediaPlayer.create(context, R.raw.g_5);
-                        }
-                }
-            case 'A':
-                switch (noteName.charAt(1)) {
-                    case '_':
-                        switch (noteName.charAt(2)) {
-                            case '4':
-                                mediaPlayer = MediaPlayer.create(context, R.raw.a_4);
-                            case '5':
-                                mediaPlayer = MediaPlayer.create(context, R.raw.a_5);
-                        }
-                    default:
-                        switch (noteName.charAt(2)) {
-                            case '4':
-                                mediaPlayer = MediaPlayer.create(context, R.raw.a4);
-                            case '5':
-                                mediaPlayer = MediaPlayer.create(context, R.raw.a5);
-                        }
-                }
-            case 'B':
-                switch (noteName.charAt(1)) {
-                    case '4':
-                        mediaPlayer = MediaPlayer.create(context, R.raw.b4);
-                    case '5':
-                        mediaPlayer = MediaPlayer.create(context, R.raw.b5);
-                }
+    private boolean isLoaded() {
+        return this.soundPool.isLoaded();
+    }
+
+    public void playSong() throws InterruptedException {
+        List<List<Note>> chords = this.song.getChords();
+        List<Thread> threads = createChordThreads(chords);
+
+        // Begin playing song
+        for (Thread thread: threads) {
+            thread.start();
+            thread.join(); // wait for thread to finish
         }
     }
 
-    public void playNote() {
-        if (mediaPlayer != null) {
-            mediaPlayer.start();
-        }
-
-        stopNote();
-    }
-
-    public void stopNote() {
-        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mp) {
-                if (mediaPlayer != null && mediaPlayer.isPlaying()) {
-                    mediaPlayer.stop();
-                    mediaPlayer.release();
-                    mediaPlayer = null;
+    private List<Thread> createChordThreads(List<List<Note>> chords) {
+        List<Thread> threads = new ArrayList<>();
+        for (List<Note> chord: chords) {
+            threads.add(new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        playChord(chord);
+                    } catch (BrokenBarrierException e) {
+                        Log.e(TAG, "Issue with broken barrier", e);
+                    } catch (InterruptedException e) {
+                        Log.e(TAG, "Issue with interrupted thread", e);
+                    }
                 }
-            }
-        });
+            }));
+        }
+        return threads;
     }
 
-    public void playSong(Song song) {
-        List<Note> notes = song.getNotes();
-        for (Note note :notes) {
-            createNote(note);
-            playNote();
+    private void playChord(List<Note> chord) throws BrokenBarrierException, InterruptedException {
+        final CyclicBarrier gate = new CyclicBarrier(chord.size() + 1);
+
+        List<Thread> threads = createNoteThreads(gate, chord);
+        for (Thread thread: threads) {
+            thread.start();
         }
+
+        // release all barriers
+        gate.await();
+    }
+
+    private List<Thread> createNoteThreads(CyclicBarrier gate, List<Note> chord) {
+        List<Thread> threads = new ArrayList<>();
+        int[] sounds = soundPool.getSounds();
+        for (Note note: chord) {
+            String noteName = note.getNoteName();
+            int index = Constants.SOUNDS_MAP.get(noteName);
+            threads.add(new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        gate.await(); // barrier: blocks threads from playing note
+                        soundPool.play(sounds[index], 1, 1, 0, 0, 1);
+                    } catch (BrokenBarrierException e) {
+                        Log.e(TAG, "Issue with broken barrier", e);
+                    } catch (InterruptedException e) {
+                        Log.e(TAG, "Issue with interrupted thread", e);
+                    }
+                }
+            }));
+        }
+        return threads;
     }
 }
