907e0aa7a8f4a7644e4bd0dda319ab8fd53102ab
Mateo Martinez <mateom@fb.com>
2021-07-19 15:22:00 -0700

    added loading and gettermethods

--- app/src/main/java/com/codepath/quarterstep/utils/MySoundPool.java
+++ app/src/main/java/com/codepath/quarterstep/utils/MySoundPool.java
@@ -7,16 +7,32 @@ import com.codepath.quarterstep.R;
 
 public class MySoundPool extends SoundPool {
     private int[] sounds;
+    private boolean loaded;
 
     public MySoundPool(int maxStreams, int streamType, int srcQuality) {
         super(maxStreams, streamType, srcQuality);
+        this.loaded = false;
     }
 
-    // Move to arrangement fragment when ready
     public void loadSounds(Context context, int[] sounds) {
-        this.sounds = new int[24];
+        this.sounds = new int[Constants.NUM_NOTES];
         for (int i = 0; i < sounds.length; i++) {
             this.sounds[i] = super.load(context, sounds[i], 1);
         }
+
+        this.setOnLoadCompleteListener(new OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                loaded = true;
+            }
+        });
+    }
+
+    public boolean isLoaded() {
+        return this.loaded;
+    }
+
+    public int[] getSounds() {
+        return this.sounds;
     }
 }
