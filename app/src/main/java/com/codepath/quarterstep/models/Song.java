9673f60b8c71a4da4df8b4bc95bc161b691c6262
Mateo Martinez <mateom@fb.com>
2021-07-19 15:20:49 -0700

    song model

--- app/src/main/java/com/codepath/quarterstep/models/Song.java
+++ app/src/main/java/com/codepath/quarterstep/models/Song.java
@@ -7,23 +7,41 @@ import java.util.List;
 
 public class Song {
     private Context context;
-    private List<Note> notes;
+    private List<List<Note>> chords;
 
     public Song(Context context) {
         this.context = context;
-        this.notes = new ArrayList<>();
+        this.chords = new ArrayList<>();
     }
 
-    public Song(Context context, List<Note> notes) {
+    public Song(Context context, List<List<Note>> rawChords) {
         this.context = context;
-        this.notes = notes;
+        this.chords = extractChords(rawChords);
     }
 
-    public void addNote(Note note) {
-        notes.add(note);
+    public List<List<Note>> getChords() {
+        return this.chords;
     }
 
-    public List<Note> getNotes() {
-        return notes;
+    public void addChord(List<Note> rawChord) {
+        this.chords.add(extractOneChord(rawChord));
+    }
+
+    private List<Note> extractOneChord(List<Note> notes) {
+        List<Note> chord = new ArrayList<>();
+        for (Note note: notes) {
+            if (note.getNoteName() != null) {
+                chord.add(note);
+            }
+        }
+        return chord;
+    }
+
+    private List<List<Note>> extractChords(List<List<Note>> rawChords) {
+        List<List<Note>> extractedChords = new ArrayList<>();
+        for (List<Note> rawChord: rawChords) {
+            extractedChords.add(extractOneChord(rawChord));
+        }
+        return extractedChords;
     }
 }
