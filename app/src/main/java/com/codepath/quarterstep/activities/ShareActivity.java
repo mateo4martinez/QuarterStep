607e00926c286c53f5e9f107a5d8c3e169a09f2e
Mateo Martinez <mateom@fb.com>
2021-07-26 16:51:16 -0700

    user can now share a song to the feed. can play song in share activity

--- app/src/main/java/com/codepath/quarterstep/activities/ShareActivity.java
+++ app/src/main/java/com/codepath/quarterstep/activities/ShareActivity.java
@@ -2,9 +2,16 @@ package com.codepath.quarterstep.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -16,12 +23,18 @@ import com.codepath.quarterstep.R;
 import com.codepath.quarterstep.models.Note;
 import com.codepath.quarterstep.models.Post;
 import com.codepath.quarterstep.models.Song;
+import com.codepath.quarterstep.utils.Constants;
+import com.codepath.quarterstep.utils.SongPlayer;
 import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
 import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class ShareActivity extends AppCompatActivity {
@@ -35,12 +48,17 @@ public class ShareActivity extends AppCompatActivity {
     private EditText etCaption;
     private Button btnFavorite;
     private Button btnShare;
+    private SoundPool soundPool;
+    private SongPlayer songPlayer;
     private Song song;
+    private String songString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
+
+        Constants.LOAD_SOUNDS(this);
 
         ivImage = findViewById(R.id.ivImage);
         ibPlay = findViewById(R.id.ibPlay);
@@ -51,41 +69,95 @@ public class ShareActivity extends AppCompatActivity {
         btnFavorite = findViewById(R.id.btnFavorite);
         btnShare = findViewById(R.id.btnShare);
 
-        // get songString from intent
-        //List<List<Note>> rawSong = Song.convertToRawSong() // pass in songString
-        // put rawSong into 'song' object
-        // build new songPlayer, loadsounds, add song into it
-        // set onclicklistener so when imagebutton clicked, play song
+        // User can click enter and be done with edit text field instead of creating a newline
+        etCharacteristics.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        etCharacteristics.setRawInputType(InputType.TYPE_CLASS_TEXT);
+        etCaption.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        etCaption.setRawInputType(InputType.TYPE_CLASS_TEXT);
+
+        Date currentTime = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+        String date = dateFormat.format(currentTime);
+        tvCreatedAt.setText(date);
+
+        songString = getIntent().getStringExtra(Constants.STRING_KEY);
+        List<List<Note>> rawSong = Song.convertToRawSong(songString);
+        song = new Song(rawSong);
+        songPlayer = buildSongPlayer();
+        songPlayer.addSong(song);
+
+        ibPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    songPlayer.playSong();
+                } catch (InterruptedException e) {
+                    Log.e(TAG, "Issue with playing song.", e);
+                }
+            }
+        });
 
         btnShare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                String songName = etSongName.getText().toString();
+                saveSong(songString, currentUser, songName);
+
                 String characteristics = etCharacteristics.getText().toString();
                 String caption = etCaption.getText().toString();
-                ParseUser currentUser = ParseUser.getCurrentUser();
-                // extract and set song characteristics here
-                // save song in parse database here
-                savePost(characteristics, caption, currentUser, ""); // send songString here
+                savePost(characteristics, caption, currentUser, songName, songString);
             }
         });
     }
 
-    private void savePost(String characteristics, String caption, ParseUser currentUser, String songString) {
+    private void saveSong(String songString, ParseUser currentUser, String songName) {
+        Song song = new Song();
+        song.setSong(songString);
+        song.setUser(currentUser);
+        song.setName(songName);
+        song.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Error while saving song", e);
+                    Toast.makeText(ShareActivity.this, "Error while saving song!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void savePost(String characteristics, String caption, ParseUser currentUser, String songName, String songString) {
         Post post = new Post();
         post.setCharacteristics(characteristics);
         post.setCaption(caption);
-        post.setSong(songString);
         post.setUser(currentUser);
+        post.setSong(songString);
+        post.setName(songName);
         post.saveInBackground(new SaveCallback() {
             @Override
             public void done(ParseException e) {
                 if (e != null) {
-                    Log.e(TAG, "Error while saving", e);
-                    Toast.makeText(ShareActivity.this, "Error while saving!", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, "Error while saving post", e);
+                    Toast.makeText(ShareActivity.this, "Error while saving post!", Toast.LENGTH_SHORT).show();
                     return;
                 }
+                goMainActivity();
                 finish();
             }
         });
+    }
+
+    private void goMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    private SongPlayer buildSongPlayer() {
+        // must set audioManager here since this is a different activity
+        AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
+
+        return new SongPlayer(this, Constants.SOUNDPOOL);
     }
 }
